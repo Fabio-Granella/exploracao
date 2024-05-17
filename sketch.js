0f73d354867a74bfdd1685009c38e811c1cbf7dc
Fabio-Granella <168829097+Fabio-Granella@users.noreply.github.com>
2024-05-17 09:40:21 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background("DarkSlateBlue");
+  //background("blue");
+  //background("#0000FF");
+  //background(0,0,255);
+
+  text("Professores", 200, 200);
+  fill(color("#FFFFF"));
+  textAlign(CENTER, CENTER);
+  textSize(64);
+     
+}
